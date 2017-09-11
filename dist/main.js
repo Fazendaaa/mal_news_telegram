f5d37f1a2e8f9e70f0fd0095017c853df9439d54
Fazendaaa <lucas.carotta@outlook.com>
2017-09-10 22:37:14 -0300

    first commit.

---
+++ dist/main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var _twitter = require('twitter');
+
+var _twitter2 = _interopRequireDefault(_twitter);
+
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
+var _telegraf = require('telegraf');
+
+var _telegraf2 = _interopRequireDefault(_telegraf);
+
+var _nodeSchedule = require('node-schedule');
+
+var _nodeSchedule2 = _interopRequireDefault(_nodeSchedule);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv2.default.config();
+
+const bot = new _telegraf2.default(process.env.BOT_TOKEN);
+const telegram = new _telegraf2.default.Telegram(process.env.BOT_TOKEN, {
+    agent: null,
+    webhookReply: true
+});
+const client = new _twitter2.default({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+let lastest_id = undefined;
+
+bot.use(_telegraf2.default.log());
+bot.use(_telegraf2.default.memorySession());
+bot.startPolling();
+
+// Runs each half hour.
+const job = _nodeSchedule2.default.scheduleJob('00,30 * * * *', () => {
+    const serverTime = new Date(Date.now());
+    const mal_params = {
+        screen_name: 'myanimelist',
+        exclude_replies: true,
+        include_rts: false,
+        since_id: lastest_id
+    };
+
+    console.log(`[${serverTime.toString()}] Running content notifications.`);
+    client.get('statuses/user_timeline', mal_params, (error, tweets, response) => {
+        if (error) console.log(error);else {
+            tweets.forEach(tweet => {
+                telegram.sendMessage(process.env.CHANNEL_ID, tweet.text);
+            });
+            lastest_id = tweets[tweets.length - 1].id;
+        }
+    });
+});
---
+++ src/main.js
@@ -0,0 +1,46 @@
+import Twitter from 'twitter';
+import dotenv from 'dotenv';
+import Telegraf from 'telegraf';
+import schedule from 'node-schedule';
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const telegram = new Telegraf.Telegram(process.env.BOT_TOKEN, {
+    agent: null,
+    webhookReply: true
+});
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+let lastest_id = undefined;
+
+bot.use(Telegraf.log());
+bot.use(Telegraf.memorySession());
+bot.startPolling();
+
+// Runs each half hour.
+const job = schedule.scheduleJob('00,30 * * * *', () => {
+    const serverTime = new Date(Date.now());
+    const mal_params = {
+        screen_name: 'myanimelist',
+        exclude_replies: true,
+        include_rts: false,
+        since_id: lastest_id
+    };
+    
+    console.log(`[${serverTime.toString()}] Running content notifications.`);
+    client.get('statuses/user_timeline', mal_params, (error, tweets, response) => {
+        if (error) console.log(error);
+
+        else {
+            tweets.forEach(tweet => {
+                telegram.sendMessage(process.env.CHANNEL_ID, tweet.text)
+            });
+            lastest_id = tweets[tweets.length-1].id;
+        }
+    });
+});
